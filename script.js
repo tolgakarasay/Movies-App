4a8ffcc3d596338fefb682b02b2cd3d52fad1e5e
Tolga Karasay <tolgakarasay@gmail.com>
2021-02-22 21:54:45 +0300

    functioning website

--- script.js
+++ script.js
@@ -5,11 +5,12 @@ const SEARCHAPI =
 "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=furious";
  */
 
- /*
+
 const API_key = "04c35731a5ee918f014970082a0088b1";
 const API_home = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
-const API_search = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=beautiful";
+const API_search_prefix = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 
+/*
 async function getMovies(url){
     const response = await fetch(url).then(response => response.json());
     console.log(response);
@@ -27,6 +28,37 @@ async function getMovies(url){
 
     return response;
 };
+*/
 
 getMovies(API_home);
-*/
+
+const main = document.querySelector("main");
+
+
+async function getMovies(url) {
+    const response = await fetch(url).then(response => response.json());
+    console.log(response);
+
+    main.innerHTML="";
+
+    response.results.map((movie) => {
+        const movieContainer = document.createElement("div");
+        movieContainer.classList.add("movie-container");
+        movieContainer.innerHTML =
+            `<img src="https://image.tmdb.org/t/p/w500${movie.poster_path}">
+            <div class="info">
+                <span class="movie-title">${movie.title}</span>
+                <span class="imdb-score">${movie.vote_average}</span>
+            </div>`;
+        main.appendChild(movieContainer);
+    })
+}
+
+const form = document.getElementById("form");
+
+form.addEventListener("submit", (e)=>{
+    const keyword = document.getElementById("search").value;
+    const API_search  = API_search_prefix + keyword;
+    getMovies(API_search);
+})
+
